b6d5feac9e86345a89c293eae1227640f1e8f742
xiaosong_bijiben <867746360@qq.com>
2018-08-29 00:11:27 +0800

    bbb

--- src/main/resources/static/common/common.js
+++ src/main/resources/static/common/common.js
@@ -4,6 +4,7 @@ document.write('<meta name="viewport" content="width=device-width, initial-scale
 document.write('<link rel="stylesheet" href="'+projectName+'/layui/css/layui.css">')
 document.write('<link rel="stylesheet" href="'+projectName+'/css/mian.css">')
 document.write('<link rel="stylesheet" href="'+projectName+'/css/yyf256.min.css">')
+document.write('<link rel="stylesheet" href="'+projectName+'/font-awesome-4.7.0/css/font-awesome.min.css">')
 document.write('<script src="'+projectName+'/layui/layui.js"></script>')
 
 

8b66f50374fcf43641ab84b34797275204a15eba
hahah <xxx@qq.com>
2018-08-28 18:13:54 +0800

    aaa

--- src/main/resources/static/common/common.js
+++ src/main/resources/static/common/common.js
@@ -3,6 +3,7 @@ var projectName="/blog";
 document.write('<meta name="viewport" content="width=device-width, initial-scale=1">')
 document.write('<link rel="stylesheet" href="'+projectName+'/layui/css/layui.css">')
 document.write('<link rel="stylesheet" href="'+projectName+'/css/mian.css">')
+document.write('<link rel="stylesheet" href="'+projectName+'/css/yyf256.min.css">')
 document.write('<script src="'+projectName+'/layui/layui.js"></script>')
 
 
